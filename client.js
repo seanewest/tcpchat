cd5b2552b087b6a414784a928a662c1951d91c5e
Sean West <sean.e.west@gmail.com>
2014-11-30 13:26:14 -0500

    all messages to clients are notified

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 var net = require('net');
+var notifier = require('node-notifier');
 
 var ip = "localhost";
 if (process.argv.length === 3) {
@@ -9,6 +10,10 @@ var connection = net.connect({host: ip, port: 3000});
 connection.on('data', function(data) {
   var msg = data.toString();
   console.log(msg);
+  notifier.notify({
+    'title': 'TCP Message',
+    'message': data.toString()
+  });
 });
 
 process.stdin.on('data', function (data) {
